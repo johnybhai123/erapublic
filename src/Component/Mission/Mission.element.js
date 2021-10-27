13006d1e45ec7a5f39c7d02e2344ebd8cd841a2e
Umesh Namdev <67787316+umeshn95@users.noreply.github.com>
2021-10-27 10:27:38 +0100

    3

--- src/Component/Mission/Mission.element.js
+++ src/Component/Mission/Mission.element.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Directorbackground = styled.div`
-  background-image: url('https://scontent.fbho4-2.fna.fbcdn.net/v/t1.6435-9/243015080_591715952182482_2729346948280851643_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=730e14&_nc_ohc=HazgE1DC490AX-v7jIN&_nc_ht=scontent.fbho4-2.fna&oh=74dc705a47d616773b57e62f2b6d2b0d&oe=6177F722');
+  background-image: url('https://lh3.googleusercontent.com/2KJo6xQ2HB-EHG0sj2FvIlF4ZMi_Ax-hh__gcHB2iFZ2wLw0voHJGejBEjQ8BMlXnIqggjj41FzdZGjHt4GQdJzgUUyRwOgZtO6BGU7MsnRS59SrFnBx9t-aZ_FBKi16CljEuFEwpUp22tAUTHarA2nZSgKuDorCJE7AAZnoSIIME09VIpx6wDfI_yuAZHZRAMYpZgCAoVYO943IA-bDzY_YqHHaUWjvA7I_JYynI0xAD6e1CznMFxq-xO7rABIVgVVk6PGBga07WsIzfY3yiD4gA9CnOMh0rFsP80-U87OFBBigM-XFgxIdL39aT-HyeJ8XCVz7zRFXaH0R8lLqYfsSkl7ugYTJh2PhP_OQX0SNoGxvTBFrfiv1SK9feSVOfe6DnAyK5caDYWMa5sm2K-1BgDlpAg5HrXY90WB0Gnoma_wpJu78d3JnI6o9LUASuMQ2hpLXx0gGAIUI-3rcJ0r-G1mjDIMavYr4RjaXWDWDj3GsQ_BH5VVeU2Tubr_o7k25gmzrNF__bIV1msyhjzfSDuS4KvvpGDZzWAoSYRzHQbEarsBCZSO3jWYpDIQ1d48_5QapXUSicjedpAnNm7tauzf1sD5O-iZn1u-B7r2kJohlf59fudc_KUEOGjejSxYA9b5GLGk_ydmP2mQh9j87XqHAY48D1gUzZ7BNrEF3a3Tww0oDUW_-NboiCnc6u3OWoMbWeeUUHIEuJBFA2chY=w1454-h969-no?authuser=0');
   height: 80vh;
   background-repeat: no-repeat;
   background-size: cover;
--- src/Component/Vission/Vission.element.js
+++ src/Component/Vission/Vission.element.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Directorbackground = styled.div`
-  background-image: url('https://scontent.fbho4-2.fna.fbcdn.net/v/t1.6435-9/243007430_591715908849153_6866976988542216486_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=730e14&_nc_ohc=YNogieVnyVsAX-weF3Q&_nc_ht=scontent.fbho4-2.fna&oh=d1dc883c135f884b674f06d5b7855e0e&oe=6178C5D1');
+  background-image: url('https://lh3.googleusercontent.com/vVUqxU2fMGEBa7N6c9P9H_29oh6EMYJknFF9nyG3asZ9vN0Hy5BAa4aUUYYfv9qOV-rl9DCLTnD2ANRQzBa4fPqzGO7qV9Q767PSeL5bPFf0fya8FB0nDXJT-ARf77pklKRw9yc7j6hDJTkVdHodk-LuYVJBKuysXLaTOvn-imLr1zcCzBYpGg0Wbg3QNDgv4IeUyIBPSs28DBhrQ-bI3zFUf5_nXXIq3y1sUHFJ-H_8WHNaF8qyBAVI-EjBY8XlK5Jw6mnDqXPvuv4xhE3HYWbsoP_Bay-yCE5AuiNshnn_p0n_HzBIdj_jNtgm9xxuGbIuEFYfZ-5OgAQr99-cjWZSiiNwIn8QN9jl0_0m53INBJOEZAcJoSEJJYjbF8-1CEc8fXlr_rS9BbqB0f-M4BoqrnMRsr6e0KFKErZXwoO6CHrIZ_Qa89gKeeyA2dXqwGzywGazVFfTjlNaF_jR4Vrg_PIWrJWGfrtu3gjcoVzS3b8s1PHb5-o_5ttlpQkNwFVJrIcE-UTvAlI_LQeIVTD0fLhhnrC5HG0QIc8iTGwCdN1W_B_DU75LtdLGpZz4AaC1puR5FmjwipDwzFB4TC0WvHI8b4kIr9ZubtG61DGirhNKpAFn0yqUKsPh4c6O1YJOvFCLxrJiNAn-ezKNznByN3pf5XKKMIWkKZeTzxY0QV8a7zhtKsovultZNd8MHAbVjvVqNaW3nXR7BeU61FyQ=w1454-h969-no?authuser=0');
   height: 80vh;
   background-repeat: no-repeat;
   background-size: cover;
