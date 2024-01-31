8027f47e489b821d4942886dd993f4b526a1bea7
johnybhai123 <71865950+johnybhai123@users.noreply.github.com>
2024-01-31 17:10:24 +0530

    Update GalleryPress.js

--- src/Component/Gallery/GalleryPress.js
+++ src/Component/Gallery/GalleryPress.js
@@ -12,24 +12,7 @@ function GalleryPress() {
           <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687974/5_mc6l0l.jpg"></Img>
           <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687967/DSC_8275_rkjekp.jpg"></Img>
           <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687949/DSC_7914_e8kr5f.jpg"></Img>
-          <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687949/DSC_7914_e8kr5f.jpg"></Img>
-          <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687954/DSC_8139_xrk8nv.jpg"></Img>
-          <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687954/DSC_8139_xrk8nv.jpg"></Img>
-          <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687956/DSC_7948_ywl6om.jpg"></Img>
-          <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687969/DSC_8321_v6mnyx.jpg"></Img>
-          <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687941/DSC_7919_jsuxdb.jpg"></Img>
-          <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687935/4_leybe5.jpg"></Img>
-          <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687940/DSC_7827_bk7qw4.jpg"></Img>
-          <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687971/DSC_8207_l0r9x0.jpg"></Img>
-          <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687950/DSC_7869_iveovl.jpg"></Img>
-          <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687949/DSC_7914_e8kr5f.jpg"></Img>
-          <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687941/DSC_7919_jsuxdb.jpg"></Img>
-          <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687974/DSC_8026_ulfcdd.jpg"></Img>
-          <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687967/DSC_8275_rkjekp.jpg"></Img>
-          <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687969/DSC_8321_v6mnyx.jpg"></Img>
-          <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687974/DSC_8026_ulfcdd.jpg"></Img>
-          <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687949/DSC_7914_e8kr5f.jpg"></Img>
-        </Subgrid>
+                  </Subgrid>
       </div>
     );
 }
