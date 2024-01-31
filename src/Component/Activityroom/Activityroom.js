ce5da6f2d5b049cf93e95aefdf5181ff5edf564c
johnybhai123 <71865950+johnybhai123@users.noreply.github.com>
2024-01-31 20:45:27 +0530

    Update Activityroom.js

--- src/Component/Activityroom/Activityroom.js
+++ src/Component/Activityroom/Activityroom.js
@@ -6,8 +6,8 @@ function Kindergarten() {
             <h1 style={{color:'green'}}>Activity & Music Room</h1>
             <hr />
           <div style={{display:'flex',flexWrap:'wrap'}}>
-          <img src='https://drive.google.com/uc?id=1E8AKtKjn0emHhi3hQah5U9rN2dPXBT4p' style={{height:'400px',width:'350px',display:'flex',margin:'auto',border:'1px solid grey',padding:'10px'}} alt="" />
-          <img src='hhttps://drive.google.com/uc?id=19QGs_MUWMf2ndBn8EuifEj6IXBNSCZK-' style={{height:'400px',width:'350px',display:'flex',margin:'auto',border:'1px solid grey',padding:'10px'}} alt="" />
+          <img src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687983/IMG_1534_qhkepq.jpg' style={{height:'400px',width:'350px',display:'flex',margin:'auto',border:'1px solid grey',padding:'10px'}} alt="" />
+          <img src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687980/IMG_1524_fpw6ef.jpg' style={{height:'400px',width:'350px',display:'flex',margin:'auto',border:'1px solid grey',padding:'10px'}} alt="" />
           
           </div>
             <p style={{padding:'40px 0 0 0'}}>The School is equipped with an Art Room where the creative surge of the student finds expression in varying forms. It provides a whetstone then their budding talents. The students are geared to excel in their artistic caliber by a team of experts. The Art room boasts of a proud collection of artwork from KGI to Grade XII.
