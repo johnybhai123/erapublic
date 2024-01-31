cd19bb4627a1814a173f39c288c383a6dc20cac7
johnybhai123 <71865950+johnybhai123@users.noreply.github.com>
2024-01-31 17:07:36 +0530

    Update Gallerimage.js

--- src/Component/Gallery/Gallerimage.js
+++ src/Component/Gallery/Gallerimage.js
@@ -7,12 +7,12 @@ function Gallerimage() {
   return (
     <div style={{ padding: "60px" }}>
       <Subgrid>
-        <Link to='/annual'><Img src='https://drive.google.com/uc?id=1bMaVWk4cSUYfQ-aIF9N_plZs6bo0Szh1'></Img></Link>
-        <Link to='/annual'><Img src='https://drive.google.com/uc?id=1g3H-08V5Y-AbvCj9hrIoYR_t_2jo4waz'></Img></Link>
-        <Link to='/annual'><Img src='https://drive.google.com/uc?id=11obVyNMqMisvCWET5ill3mD5AtqHGKpX'></Img></Link>
-        <Link to='/annual'><Img src='https://drive.google.com/uc?id=1CErwxCkY60yfeE5UQLJ7w6SBKnwNiBCA'></Img></Link>
-        <Link to='/annual'><Img src='https://drive.google.com/uc?id=1-qFbwx0rcryi0vINmWNt3JexOAtgAljO'></Img></Link>
-        <Link to='/annual'><Img src='https://drive.google.com/uc?id=1R-QvTCpKog7ymh98Hkaz4EupwYpyQAne'></Img></Link>
+        <Link to='/annual'><Img src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687977/IMG_1491_j5subk.jpg'></Img></Link>
+        <Link to='/annual'><Img src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687991/IMG_1494_rrb3gw.jpg'></Img></Link>
+        <Link to='/annual'><Img src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687974/IMG_1461_olfjd6.jpg'></Img></Link>
+        <Link to='/annual'><Img src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687984/IMG_1452_dre5ua.jpg'></Img></Link>
+        <Link to='/annual'><Img src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687984/IMG_1452_dre5ua.jpg'></Img></Link>
+        <Link to='/annual'><Img src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687962/IMG_0959_zxwzoo.jpg'></Img></Link>
       
       </Subgrid>
     </div>
