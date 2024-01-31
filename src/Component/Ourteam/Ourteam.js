5be1abead89a48def1046df7a3727b74a33ad1ae
johnybhai123 <71865950+johnybhai123@users.noreply.github.com>
2024-01-31 18:43:50 +0530

    Update Ourteam.js

--- src/Component/Ourteam/Ourteam.js
+++ src/Component/Ourteam/Ourteam.js
@@ -14,13 +14,13 @@ function Ourteam() {
            {/* <Teamimage></Teamimage>  */}
            <Heading/>
            <Rowcolumn33>
-              <Link to='/Director' style={{textDecoration:'none'}}>  <Teampeople image='https://drive.google.com/uc?id=1NMB1iVrHDGSiCuR0iaEuqUu-XcvH7K-9' name='Sarita Budana' designation='Director' /></Link>
+              <Link to='/Director' style={{textDecoration:'none'}}>  <Teampeople image='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687932/Director_Sarita_Budana_rozova.jpg' name='Sarita Budana' designation='Director' /></Link>
       
-              <Link to='/Md'  style={{textDecoration:'none'}}>  <Teampeople image='https://drive.google.com/uc?id=1WwmXR0U-j-cbY8JwkuOLjUgYaz-LjRFH' name='Ratna Kusumakar' designation='Managing Director'/></Link>
+              <Link to='/Md'  style={{textDecoration:'none'}}>  <Teampeople image='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687990/Managing_Director_yejcqq.jpg' name='Ratna Kusumakar' designation='Managing Director'/></Link>
       
-              <Link to='/president'  style={{textDecoration:'none'}}>  <Teampeople image='https://drive.google.com/uc?id=1ztndTs8a2PQIi2ezMl_5uNuiiV-09Zav' name='Deepak Kusumakar' designation='President'/></Link>
+              <Link to='/president'  style={{textDecoration:'none'}}>  <Teampeople image='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687985/President__Deepak_Kusumakar_pihm8s.jpg' name='Deepak Kusumakar' designation='President'/></Link>
       
-              <Link to='/principal'  style={{textDecoration:'none'}}>  <Teampeople image='https://drive.google.com/uc?id=1Z_O9y6waQxrUGLPaPw4x-X_ZXQ4t1ysJ' name='Archana Tiwari' designation='Principal'/></Link>
+              <Link to='/principal'  style={{textDecoration:'none'}}>  <Teampeople image='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706706746/princi_nz6yvy.jpg' name='Archana Tiwari' designation='Principal'/></Link>
               
              
            </Rowcolumn33>
