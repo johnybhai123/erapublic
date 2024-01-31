990e031822dd7a81815253b02edca1b3cc3c4b0d
johnybhai123 <71865950+johnybhai123@users.noreply.github.com>
2024-01-31 20:46:50 +0530

    Update Labs.js

--- src/Component/Labs/Labs.js
+++ src/Component/Labs/Labs.js
@@ -9,10 +9,10 @@ function Labs() {
             <h1 style={{color:'green'}}>Science labs</h1>
             <hr />
            <div style={{display:'flex',flexWrap:'wrap',gap:'20px',justifyContent:'center'}}>
-           <img src='https://drive.google.com/uc?id=1FO45KKK9vBk5QekNDgm9AKx2TN-25Abc' style={{height:'300px',width:'300px', border:'1px solid black', padding:'10px'}} alt="" />
+           <img src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687932/chemistry_lab_anlesb.jpg' style={{height:'300px',width:'300px', border:'1px solid black', padding:'10px'}} alt="" />
            
-            <img src='https://drive.google.com/uc?id=1R-QvTCpKog7ymh98Hkaz4EupwYpyQAne' style={{height:'300px',width:'300px', border:'1px solid black', padding:'10px'}} alt="" />
-            <img src='https://drive.google.com/uc?id=15NByCygIGjQbNoaZjdjHhCd6VzpamhCP' style={{height:'300px',width:'300px', border:'1px solid black', padding:'10px'}} alt="" />
+            <img src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687962/IMG_0959_zxwzoo.jpg' style={{height:'300px',width:'300px', border:'1px solid black', padding:'10px'}} alt="" />
+            <img src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687965/IMG_0963_luhjkh.jpg' style={{height:'300px',width:'300px', border:'1px solid black', padding:'10px'}} alt="" />
            </div>
             <p style={{padding:'40px 0 0 0'}}>The science labs in the school are well equipped with apparatus and chemicals and provide first –hand experience of all practical activities to each student. These labs have a unique collection of charts, models, slides and specimens as per the syllabus of each class. </p>
 
