1aa208e42082c6f92762200578d20eddc991aeb5
johnybhai123 <71865950+johnybhai123@users.noreply.github.com>
2024-05-02 21:27:45 +0530

    Update Annualfunction.js

--- src/Component/Annualfunction/Annualfunction.js
+++ src/Component/Annualfunction/Annualfunction.js
@@ -20,14 +20,14 @@ function Annualfunction() {
 
             <Div style={{display:'flex',flexWrap:'wrap'}}>
 
-                        <img style={{height:'300px',width:'300px'}} src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687948/DSC_8006_sto6cu.jpg' alt="" />
-                        <img style={{height:'300px',width:'300px'}} src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687959/DSC_8233_uqclx1.jpg' alt="" />
-                        <img style={{height:'300px',width:'300px'}} src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687956/DSC_7948_ywl6om.jpg' alt="" />
-                        <img style={{height:'300px',width:'300px'}} src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687969/DSC_8321_v6mnyx.jpg' alt="" />
-                        <img style={{height:'300px',width:'300px'}} src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687941/DSC_7919_jsuxdb.jpg' alt="" />
-                        <img style={{height:'300px',width:'300px'}} src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687940/DSC_7827_bk7qw4.jpg' alt="" />
-                        <img style={{height:'300px',width:'300px'}} src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687957/DSC_8149_gzqmhu.jpg' alt="" />
-                        <img style={{height:'300px',width:'300px'}} src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687950/DSC_7869_iveovl.jpg' alt="" />
+                        <img style={{height:'300px',width:'300px'}} src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1714665110/MAAA1332_eqt4an.jpg' alt="" />
+                        <img style={{height:'300px',width:'300px'}} src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1714665107/MAAA1621_p7fci0.jpg' alt="" />
+                        <img style={{height:'300px',width:'300px'}} src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1714665106/MAAA1485_ttslly.jpg' alt="" />
+                        <img style={{height:'300px',width:'300px'}} src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1714665088/WhatsApp_Image_2023-12-16_at_12.50.03_PM_s7yi9p.jpg' alt="" />
+                        <img style={{height:'300px',width:'300px'}} src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1714665072/MAAA1208_doufj5.jpg' alt="" />
+                        <img style={{height:'300px',width:'300px'}} src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1714665077/MAAA1572_maufv1.jpg' alt="" />
+                        <img style={{height:'300px',width:'300px'}} src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1714665087/MAAA0928_bysgfz.jpg' alt="" />
+                        <img style={{height:'300px',width:'300px'}} src='https://res.cloudinary.com/dvmqfke8w/image/upload/v1714665087/MAAA0928_bysgfz.jpg' alt="" />
                 </Div>
 
 
