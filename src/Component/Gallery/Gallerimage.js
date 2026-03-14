a66d74002c0f2c206ba28c237ee2d0929e3c344b
DarwinAnalytics <darwinanalytic@gmail.com>
2026-03-15 00:22:11 +0530

    actual gallery

--- src/Component/Gallery/Gallerimage.js
+++ src/Component/Gallery/Gallerimage.js
@@ -7,12 +7,16 @@ function Gallerimage() {
   return (
     <div style={{ padding: "60px" }}>
       <Subgrid>
-        <Link to='/annual'><Img src='\website data for 2025-26\annual function\DSC_2129 annual function.JPG'></Img></Link>
-        <Link to='/annual'><Img src='\website data for 2025-26\annual function\DSC_2615kawali.JPG'></Img></Link>
-        <Link to='/annual'><Img src='\website data for 2025-26\annual function\DSC_2695 AF.JPG'></Img></Link>
-        <Link to='/annual'><Img src='\website data for 2025-26\annual function\DSC_2700 annual.JPG'></Img></Link>
-        <Link to='/annual'><Img src='\website data for 2025-26\annual function\MAAA0030.JPG'></Img></Link>
-        <Link to='/annual'><Img src='\website data for 2025-26\annual function\MAAA0489.JPG'></Img></Link>
+  <Img src="\website data for 2025-26\12_photo gallary\WhatsApp_Image_2026-03-11_at_11_01_26_AM.jpeg"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\WhatsApp_Image_2026-01-27_at_6_32_12_PM.jpeg"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\WhatsApp_Image_2026-01-27_at_6_32_06_PM.jpeg"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\96061b20-a761-4499-b6dd-90d0de87f08b.jpg"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\MAAA7417.JPG"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\4A0A1679.JPG"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\IMG_20250710_115542.jpg"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\556778133_1542057120336781_6221986535174354023_n.jpg"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\514961586_1470284847514009_4639526485545232777_n.jpg"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\WhatsApp_Image_2025-12-06_at_3_47_22_PM.jpeg"></Img>
         
       
       </Subgrid>
