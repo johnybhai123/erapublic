0669ac09922194e8cdeb5d99c10110e8105e29e8
DarwinAnalytics <darwinanalytic@gmail.com>
2026-03-15 00:32:36 +0530

    celebration photo

--- src/Component/Gallery/GalleryPress.js
+++ src/Component/Gallery/GalleryPress.js
@@ -6,16 +6,16 @@ function GalleryPress() {
     return (
       <div style={{ padding: "20px" }}>
         <Subgrid>
-          <Img src="\website data for 2025-26\12_photo gallary\WhatsApp_Image_2026-03-11_at_11_01_26_AM.jpeg"></Img>
-          <Img src="\website data for 2025-26\12_photo gallary\WhatsApp_Image_2026-01-27_at_6_32_12_PM.jpeg"></Img>
-          <Img src="\website data for 2025-26\12_photo gallary\WhatsApp_Image_2026-01-27_at_6_32_06_PM.jpeg"></Img>
-          <Img src="\website data for 2025-26\12_photo gallary\96061b20-a761-4499-b6dd-90d0de87f08b.jpg"></Img>
-          <Img src="\website data for 2025-26\12_photo gallary\MAAA7417.JPG"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\MAAA9070.JPG"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\MAAA85951.JPG"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\MAAA8296.JPG"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\MAAA7666.JPG"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\4A0A1900.JPG"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\4A0A1849.JPG"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\4A0A1749.JPG"></Img>
           <Img src="\website data for 2025-26\12_photo gallary\4A0A1679.JPG"></Img>
-          <Img src="\website data for 2025-26\12_photo gallary\IMG_20250710_115542.jpg"></Img>
-          <Img src="\website data for 2025-26\12_photo gallary\556778133_1542057120336781_6221986535174354023_n.jpg"></Img>
-          <Img src="\website data for 2025-26\12_photo gallary\514961586_1470284847514009_4639526485545232777_n.jpg"></Img>
-          <Img src="\website data for 2025-26\12_photo gallary\WhatsApp_Image_2025-12-06_at_3_47_22_PM.jpeg"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\4A0A1588.JPG"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\4A0A1446.JPG"></Img>
         </Subgrid>
       </div>
     );
--- src/Component/Gallery/Galleryvideo.js
+++ src/Component/Gallery/Galleryvideo.js
@@ -1,18 +1,25 @@
 import React from 'react'
-import { Subgrid } from './Gallery.element'
+import { Img, Subgrid } from './Gallery.element'
 
 function Galleryvideo() {
     return (
         <div style={{padding:'20px'}}>
         <Subgrid>
-        <iframe width="250" height="250" src="https://youtu.be/cpxjreKqpN0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="true"></iframe>
-        <iframe width="250" height="250" src="https://youtu.be/n_r_dwOAOUM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="true"></iframe>
-        <iframe width="250" height="250" src="https://youtu.be/_3Yw3LgZ9aY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="true"></iframe>
-        <iframe width="250" height="250" src="https://youtu.be/_-cicNnwrto" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="true"></iframe>
-        <iframe width="250" height="250" src="https://youtu.be/_-cicNnwrto" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="true"></iframe>
-        <iframe width="250" height="250" src="https://youtu.be/zD3jEy3gpdc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="true"></iframe>
-       
-      
+          <Img src="\website data for 2025-26\13_celebration photo\WhatsApp_Image_2026-01-27_at_6_32_58_PM__1_.jpeg"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\WhatsApp_Image_2026-01-27_at_6_32_58_PM.jpeg"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\WhatsApp_Image_2026-01-27_at_6_31_59_PM.jpeg"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\WhatsApp_Image_2026-01-27_at_6_31_54_PM.jpeg"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\IMG_20250710_115535_Bokeh.jpg"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\IMG_20250710_115542.jpg"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\556778133_1542057120336781_6221986535174354023_n.jpg"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\529791881_1497966864745807_1298375150496466295_n.jpg"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\514961586_1470284847514009_4639526485545232777_n.jpg"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\504382493_1470284490847378_3930860581584473747_n.jpg"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\516078679_1470286990847128_61971149691091329_n.jpg"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\504432909_1466139927928501_4344311834632073061_n.jpg"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\514088723_1464852261390601_5531164442213311123_n.jpg"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\487507834_1399038584638636_8634196528160168847_n.jpg"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\WhatsApp_Image_2025-12-02_at_12_01_38_PM.jpeg"></Img>
         </Subgrid>
     </div>
     )
