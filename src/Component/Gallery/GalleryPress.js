150c7ae325e96b8e5e61cc47b675ef534da8cd89
DarwinAnalytics <darwinanalytic@gmail.com>
2026-03-15 00:17:38 +0530

    galler image updated

--- src/Component/Gallery/GalleryPress.js
+++ src/Component/Gallery/GalleryPress.js
@@ -6,18 +6,19 @@ function GalleryPress() {
     return (
       <div style={{ padding: "20px" }}>
         <Subgrid>
-          <Img src="\website data for 2025-26\12_photo gallary\2.jpeg"></Img>
-          <Img src="https://res.cloudinary.com/dvmqfke8w/image/upload/v1706687948/DSC_8006_sto6cu.jpg"></Img>
-          <Img src="\website data for 2025-26\12_photo gallary\466920785_1297967191412443_8124580088633938832_n.jpg"></Img>
-          <Img src="\website data for 2025-26\12_photo gallary\466963581_1298454034697092_5093681710718692104_n.jpg"></Img>
-          <Img src="\website data for 2025-26\12_photo gallary\467395662_1300037144538781_2840541889747279706_n.jpg"></Img>
-          <Img src="\website data for 2025-26\12_photo gallary\467493494_1300037524538743_4376700392344753314_n.jpg"></Img>
-         
-                  </Subgrid>
+          <Img src="\website data for 2025-26\12_photo gallary\WhatsApp_Image_2026-03-11_at_11_01_26_AM.jpeg"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\WhatsApp_Image_2026-01-27_at_6_32_12_PM.jpeg"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\WhatsApp_Image_2026-01-27_at_6_32_06_PM.jpeg"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\96061b20-a761-4499-b6dd-90d0de87f08b.jpg"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\MAAA7417.JPG"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\4A0A1679.JPG"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\IMG_20250710_115542.jpg"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\556778133_1542057120336781_6221986535174354023_n.jpg"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\514961586_1470284847514009_4639526485545232777_n.jpg"></Img>
+          <Img src="\website data for 2025-26\12_photo gallary\WhatsApp_Image_2025-12-06_at_3_47_22_PM.jpeg"></Img>
+        </Subgrid>
       </div>
     );
 }
 
 export default GalleryPress
-
-
