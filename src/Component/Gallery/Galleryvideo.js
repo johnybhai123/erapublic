05a6a683b46a2b9ea8552a07e0672f4a839a2ee9
DarwinAnalytics <darwinanalytic@gmail.com>
2026-03-16 00:42:03 +0530

    cel photo

--- src/Component/Gallery/Galleryvideo.js
+++ src/Component/Gallery/Galleryvideo.js
@@ -5,21 +5,21 @@ function Galleryvideo() {
     return (
         <div style={{padding:'20px'}}>
         <Subgrid>
-          <Img src="\website data for 2025-26\13_celebration photo\WhatsApp_Image_2026-01-27_at_6_32_58_PM__1_.JPG"></Img>
-          <Img src="\website data for 2025-26\13_celebration photo\WhatsApp_Image_2026-01-27_at_6_32_58_PM.JPG"></Img>
-          <Img src="\website data for 2025-26\13_celebration photo\WhatsApp_Image_2026-01-27_at_6_31_59_PM.JPG"></Img>
-          <Img src="\website data for 2025-26\13_celebration photo\WhatsApp_Image_2026-01-27_at_6_31_54_PM.JPG"></Img>
-          <Img src="\website data for 2025-26\13_celebration photo\IMG_20250710_115535_Bokeh.JPG"></Img>
-          <Img src="\website data for 2025-26\13_celebration photo\IMG_20250710_115542.JPG"></Img>
-          <Img src="\website data for 2025-26\13_celebration photo\556778133_1542057120336781_6221986535174354023_n.JPG"></Img>
-          <Img src="\website data for 2025-26\13_celebration photo\529791881_1497966864745807_1298375150496466295_n.JPG"></Img>
-          <Img src="\website data for 2025-26\13_celebration photo\514961586_1470284847514009_4639526485545232777_n.JPG"></Img>
-          <Img src="\website data for 2025-26\13_celebration photo\504382493_1470284490847378_3930860581584473747_n.JPG"></Img>
-          <Img src="\website data for 2025-26\13_celebration photo\516078679_1470286990847128_61971149691091329_n.JPG"></Img>
-          <Img src="\website data for 2025-26\13_celebration photo\504432909_1466139927928501_4344311834632073061_n.JPG"></Img>
-          <Img src="\website data for 2025-26\13_celebration photo\514088723_1464852261390601_5531164442213311123_n.JPG"></Img>
-          <Img src="\website data for 2025-26\13_celebration photo\487507834_1399038584638636_8634196528160168847_n.JPG"></Img>
-          <Img src="\website data for 2025-26\13_celebration photo\WhatsApp_Image_2025-12-02_at_12_01_38_PM.JPG"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\celebrationphoto1.JPG"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\celebrationphoto2.JPG"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\celebrationphoto3.JPG"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\celebrationphoto4.JPG"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\celebrationphoto5.JPG"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\celebrationphoto7.JPG"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\celebrationphoto8.JPG"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\celebrationphoto9.JPG"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\celebrationphoto10.JPG"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\celebrationphoto11.JPG"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\celebrationphoto12.JPG"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\celebrationphoto13.JPG"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\celebrationphoto14.JPG"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\celebrationphoto15.JPG"></Img>
+          <Img src="\website data for 2025-26\13_celebration photo\celebrationphoto16.JPG"></Img>
         </Subgrid>
     </div>
     )
